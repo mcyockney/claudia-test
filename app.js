310bc0f4a633bf00d1d6ed997f794217a3e02004
Matt <matthewy@1kiwi.com>
2016-10-27 08:41:56 +0000

    Updated to reflect the basic contact details for Sign It

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const api = botBuilder((request, originalApiRequest) => {
       .then(response => {
         const user = JSON.parse(response.body)
         return [
-          `Hello ${user.first_name}. Welcome to Space Explorer! Ready to start a journey through space?`,
+          `Hello ${user.first_name}. Welcome to Sign It Signs`,
           'What can I do for you today?',
           mainMenu()
         ]
@@ -30,78 +30,26 @@ const api = botBuilder((request, originalApiRequest) => {
 
   if (request.text === 'HOURS')
         return [
-          `We are open Monday to Friday, 9am-5pm`,
+          `We are open Monday to Friday, 9am-6pm`,
           `If you need it, we can even be open longer, but you'll have to be nice`,
           new fbTemplate.button('More actions:')
             .addButton('Back to start', 'MAIN_MENU')
             .get()
         ]
       
-
-  if (request.text === 'ISS_POSITION')
-    return rp.get('https://api.wheretheiss.at/v1/satellites/25544')
-      .then(response => {
-        const ISS = JSON.parse(response.body)
+  if (request.text === 'CONTACT')
         return [
-          new fbTemplate.generic()
-            .addBubble(`International Space Station`, 'Current position')
-              .addImage(`https://maps.googleapis.com/maps/api/staticmap?center=${ISS.latitude},${ISS.longitude}&zoom=2&size=640x335&markers=color:red%7C${ISS.latitude},${ISS.longitude}`)
-              .addButton('Show website', 'http://iss.astroviewer.net')
-            .get(),
-            `International Space Station:`,
-            `- Latitude: ${ISS.latitude};\n- Longitude: ${ISS.longitude};\n- Velocity: ${ISS.velocity}kmh;\n- Altitude: ${ISS.altitude};\n- Visibility: ${ISS.visibility}`
+          `You can ask us here for help, or send us an email or phone us.`,
+          new fbTemplate.button('More actions:')
+            .addButton('Back to start', 'MAIN_MENU')
+            .get()
         ]
-      })
+      
 
-  if (request.text === 'PEOPLE_IN_SPACE')
-    return rp.get('http://api.open-notify.org/astros.json')
-      .then(response => {
-        const inSpace = JSON.parse(response.body)
-        return [
-          `There are ${inSpace.number} people in Space right now.`,
-          inSpace.people.reduce((response, person) => {
-            return response + `- ${person.name}` + ((person.craft) ? ` is on ${person.craft}` : '') + ';\n'
-          }, '')
-        ]
-      })
 
   if (request.text === 'MAIN_MENU')
     return mainMenu()
 
-
-  if (request.text === 'ABOUT_APOD')
-    return [
-      `The Astronomy Picture of the Day is one of the most popular websites at NASA. In fact, this website is one of the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.`,
-      `Each day a different image or photograph of our fascinating universe is featured, along with a brief explanation written by a professional astronomer.`,
-      new fbTemplate.button('More actions:')
-        .addButton('Show photo', 'SHOW_APOD')
-        .addButton('Visit website', 'http://apod.nasa.gov/apod/')
-        .addButton('Back to start', 'MAIN_MENU')
-        .get()
-    ]
-
-  if (request.text === 'ABOUT')
-    return [
-      `Space Explorer is simple Messenger chat bot that uses NASA's API to get the data and images about the space`,
-      `It's created for fun and also as a showcase for Claudia Bot Builder, node.js library for creating chat bots for various platform and deploying them on AWS Lambda`,
-      new fbTemplate.button('More actions:')
-        .addButton('Claudia Bot Builder', 'https://github.com/claudiajs/claudia-bot-builder')
-        .addButton('Source code', 'https://github.com/stojanovic/space-explorer-bot')
-        .get()
-    ]
-
-  if (request.text === 'CREDITS')
-    return [
-      'Claudia Bot Builder was created by Gojko Adžić, Aleksandar Simović and Slobodan Stojanović',
-      'Icons used for the bot are from the Noun Project',
-      '- Rocket icon by misirlou, \n- Satellite icon by parkjisun, \n- Curiosity Rover icon by Oliviu Stoian, \n- Monster icon by Paulo Sá Ferreira',
-      'This bot was created by Claudia Bot Builder team',
-      new fbTemplate.button('More actions:')
-        .addButton('Claudia Bot Builder', 'https://github.com/claudiajs/claudia-bot-builder')
-        .addButton('The Noun Project', 'https://thenounproject.com')
-        .addButton('Source code', 'https://github.com/stojanovic/space-explorer-bot')
-        .get()
-    ]
 })
 
 api.addPostDeployConfig('nasaApiKey', 'NASA API Key:', 'configure-app');
